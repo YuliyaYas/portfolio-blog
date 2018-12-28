c7b0ceb341c615c6a81bcbb8a470e54734aaa6cb
YuliyaYasenetska <Yuliya.Yasenetska@billboard.com>
2018-12-27 21:12:42 -0500

    redoing designs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+// import {Route, Switch} from 'react-router-dom';
+// TODO: install npm react-router-dom
 import './App.css';
+import Header from '../src/components/Header.js';
+import Home from '../src/components/Home.js';
 
 class App extends Component {
   render() {
     return (
       <div>
-      hi
+        <Header />
+        <Home />
       </div>
     );
   }

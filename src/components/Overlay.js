1f305875ce25d2ab1d0bac9d2de03a053ba47d5c
YuliyaYasenetska <Yuliya.Yasenetska@billboard.com>
2018-12-29 12:36:02 -0500

    added arrows for overlay

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -5,8 +5,10 @@ import '../stylesheet/Overlay.css';
 const Overlay1 = (props) => {
   return(
     <div className="base" >
-      <div className="shroud overlay-scroll">
+      <div className="shroud overlay-scroll box">
         <div className="overlay-popup">
+          <img class="left-arrow" src="https://image.flaticon.com/icons/svg/60/60758.svg" alt="green" />
+          <img class="right-arrow" src="https://image.flaticon.com/icons/svg/60/60758.svg" alt="red" />
           <p className="close" onClick={props.handleCloseClick}/>
           <p className="all-caps">{props.info.name}</p>
           {props.info.url ? <img id="overlay-img" src={require(`../imgs/paintings/${props.info.url}`)}/> : "" }

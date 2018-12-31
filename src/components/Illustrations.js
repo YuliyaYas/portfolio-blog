21279a66856a7ddfd775dcb4c5b33f78a08cf3e6
YuliyaYasenetska <Yuliya.Yasenetska@billboard.com>
2018-12-30 23:47:39 -0500

    rearranging illustrations

--- src/components/Illustrations.js
+++ src/components/Illustrations.js
@@ -15,45 +15,49 @@ const Illustrations = (props) =>  {
           <div className="row-grid">
             <div className="column-grid">
               <img src={require("../imgs/illustrations/atom.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/baloon1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/birds-view.png")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/blackberry.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/Flo-city.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/goat.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/colibri.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/birds-view.png")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/houseplant.png")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/Train1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/shelville1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/snaility1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/Burger-.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/dinosaur.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/gyraff2.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
             </div>
             <div className="column-grid">
-              <img src={require("../imgs/illustrations/Burger-.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/flamingo.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/camel.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/chameleon.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/gyraff2.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/heart3.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/chameleon.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/housegrowth.png")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/houses-wine.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/crab.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/houses-yard.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/Pattern.png")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
             </div>
             <div className="column-grid">
               <img src={require("../imgs/illustrations/City-on-the-hill.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/colibri.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/crab.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/houses-yard.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/Housing-projects.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/jellyfish.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/saturn1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/townhouse_insta.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/sunflower.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/turtle2.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/jellyfish.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/deer.png")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/saturn1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/Housing-projects.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/Flo-city.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/townhouse_insta.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
             </div>
             <div className="column-grid">
-              <img src={require("../imgs/illustrations/dirigible.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/fish2.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/flamingo.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/Lemur.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/orchids.png")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/snaility1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/Mushrooms.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/octopus4.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/dirigible.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/fish2.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
               <img src={require("../imgs/illustrations/shell1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/Train1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
-              <img src={require("../imgs/illustrations/turtle2.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/goat.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
+              <img src={require("../imgs/illustrations/baloon1.jpg")} title={props.illustrationsData[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} />
             </div>
           </div>
         </div>

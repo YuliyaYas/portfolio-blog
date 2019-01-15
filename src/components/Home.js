97d29aa7c5ee1188ed4387cb44df256d1e7ef8c0
YuliyaYasenetska <Yuliya.Yasenetska@billboard.com>
2019-01-14 20:10:30 -0500

    home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,13 @@ import '../stylesheet/Home.css';
 const Header = () =>  {
       return(
         <div id="home">
-          <img id="home-img" src={require("../imgs/octopus.png")}/>
+          <img id="home-img" src={require("../imgs/paintings/Snaility.jpg")}/>
           <div className="title">
-            <p>OCTOVILLE</p>
-            <i>30x30inches, acrylic on wooden panel</i>
+            <p>SNAILITY</p>
+            <i>20x20inches, acrylic on wooden panel</i>
           </div>
+          <p className="desc">The Greek poet Hesiod wrote that snails signified the time to harvest by climbing up the stems. This symbolized growth and rebirth. As women, we lead new innovations and ideas, we build new generations. We constantly grow and climb up the social ladder. Slowly but steady, small but many – may we rise up and carry our future to the brightest point.</p>
+
         </div>
       );
 };

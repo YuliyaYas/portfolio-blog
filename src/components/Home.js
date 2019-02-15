c3879823a67ba4822a994ca229e5280885a8108b
YuliyaYasenetska <Yuliya.Yasenetska@billboard.com>
2019-02-15 00:29:14 -0500

    mobile design changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,12 +4,12 @@ import '../stylesheet/Home.css';
 const Header = () =>  {
       return(
         <div id="home">
-          <p className="desc-home">The Greek poet Hesiod wrote that snails signified the time to harvest by climbing up the stems. This symbolized growth and rebirth. As women, we lead new innovations and ideas, we build new generations. We constantly grow and climb up the social ladder. Slowly but steady, small but many – may we rise up and carry our future to the brightest point.</p>
           <img id="home-img" src={require("../imgs/paintings/Snaility.jpg")}/>
           <div className="title">
             <p>REBIRTH</p>
             <i>20x20inches, acrylic on wooden panel</i>
           </div>
+          <p className="desc">The Greek poet Hesiod wrote that snails signified the time to harvest by climbing up the stems. This symbolized growth and rebirth. As women, we lead new innovations and ideas, we build new generations. We constantly grow and climb up the social ladder. Slowly but steady, small but many – may we rise up and carry our future to the brightest point.</p>
 
         </div>
       );

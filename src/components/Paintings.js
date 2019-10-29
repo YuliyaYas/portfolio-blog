1ef6e98920f0ab14007350f12c4566b2262a40f4
Yuliya <jasenetska@gmail.com>
2019-10-29 19:36:35 -0400

    added new paintings

--- src/components/Paintings.js
+++ src/components/Paintings.js
@@ -15,22 +15,28 @@ const Paintings = (props) =>  {
           <p className="click">Click on image to read more about the work</p>
           <div className="row-grid">
             <div className="column-grid">
+              <img src={require("../imgs/paintings/Sun-Earth.jpg")} title={props.paintingsData[19].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
+              <img src={require("../imgs/paintings/Grace.jpg")} title={props.paintingsData[14].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/TurtleVille.jpg")} title={props.paintingsData[0].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/Chamille.jpg")} title={props.paintingsData[1].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/City-on-clouds.jpg")} title={props.paintingsData[2].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/dou-color-Abstract.jpg")} title={props.paintingsData[3].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
             </div>
             <div className="column-grid">
+              <img src={require("../imgs/paintings/Growth.jpg")} title={props.paintingsData[15].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/Dreamworld.jpg")} title={props.paintingsData[4].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/Flamity.png")} title={props.paintingsData[5].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/FullGlass.jpg")} title={props.paintingsData[6].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
             </div>
             <div className="column-grid">
+              <img src={require("../imgs/paintings/Saturn.jpg")} title={props.paintingsData[18].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
+              <img src={require("../imgs/paintings/Hope.jpg")} title={props.paintingsData[16].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/Octo.png")} title={props.paintingsData[7].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/Snaility.jpg")} title={props.paintingsData[8].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/Tale.jpg")} title={props.paintingsData[9].name} style={{width:'100%'}} name="paintings" onClick={props.handleImageClick}/>
             </div>
             <div className="column-grid">
+              <img src={require("../imgs/paintings/Up.jpg")} title={props.paintingsData[17].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/WorldsEnergy.jpg")} title={props.paintingsData[10].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/Master-copy-of-Louisa-Matthiasdottirs-work.jpg")} title={props.paintingsData[11].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>
               <img src={require("../imgs/paintings/Tree1.jpg")} title={props.paintingsData[12].name} name="paintings" style={{width:'100%'}} onClick={props.handleImageClick}/>

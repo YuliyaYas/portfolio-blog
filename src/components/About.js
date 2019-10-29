6ea6bc1188ff8f33a4f4902f7251a3bd58deb5a8
Yuliya <jasenetska@gmail.com>
2019-10-29 19:41:38 -0400

    updated about page

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,18 @@ const About = () =>  {
         <div>
           <div id="about">
             <img id="profile-pic" src={require("../imgs/portrait1.jpg")} />
-            <p>Yuliya was born and raised in Ukraine, and currently resides in New York City.
+            <p>Yuliya Yasenetska currently resides in New York City, but was born and raised in Lviv, Ukraine.
             </p>
-            <p>Yuliya is strongly interested in nature, civilization, and humanity. What is a human being? How does civilization interact with nature? Each animal, building or technology in her works symbolize personalities, feelings, thoughts and emotions that we deal with everyday. The classic theme of understanding by means of imagination and feeling rather than analytical kind of thinking.
+            <p>Yuliya enjoys experimenting with different mediums. She feels that painting, drawing, digital art, printmaking and the combination of these techniques is the representation of variation of things that happens in our life. Humans constantly change, and as an artist, Yuliya continuously seeks improvement and varied methods to express herself.
             </p>
-            <p>Each place she draws are real places with personal significance in one way or another. Buildings, that often appear in her works are symbols of what we are building or have built in our lives. They are representations of our past, present and future. The use of color is natural in its simplicity but also artificial enough in its purity and vividness. The colors are bright and contrasting, where dark lines represent ordinary days, where grey masses of people follow daily routine.</p>
-            <p>The use of different mediums, such as painting, drawing, digital art, printmaking and the combination of some of these techniques together is a representation of variety of things that happens in our life. Humans constantly change and search for variety in their lives, and as an artist Yuliya constantly looks for improvement and different ways to express herself.</p>
+            <p>Yuliya focuses on how her artwork looks and feels, not on ingraining hidden meanings into the art. When children look at the artwork - they don’t question ideas, or start debating on philosophical topics. All they see is colors, shapes, and their own view of the image; blended with their own naive vivid imaginations. She hopes to capture that carefree imagination in her paintings without enforcing any predefined connotations.
+            </p>
+            <p>Yuliya is tremendously inspired by nature, civilization, and humanity. What does it mean to be a human being? How does (and should) civilization interact with nature? Each element in her works reflect personalities, feelings, thoughts, and emotions that she experiences in her life. The classic theme of her art is the intersection of both imagination, and the understanding of our place in this world.
+            </p>
+            <p>Each place she depicts has real personal significance to her. Buildings which often appear in her works are symbols of what everyday humans are building or have built in our lives. They are representations of our past, present, and future. The use of color is fundamental to its simplicity, but also artificially spontaneous in its purity and vividness. The colors are bright and contrasting, the dark lines represent ordinary days, while the grey masses embody the daily routines and rituals that everyone must follow.
+            </p>
+            <p>In this highly connected modern world, we are constantly and disproportionately exposed to negative emotions. Social media/news thrive and grow of those same negative emotions. This only adds to our life's frustrations such as from work, relationships, and other struggles. Through the light and colorful depictions of her artwork she tries to awaken forgotten joy. Memories of joys similar to what we've all felt as children, like when an ice cream truck tune came playing down the street. Painting was the one thing that always remained full of positive emotions for Yuliya and helped her overcome life's pains, struggles, and personal depression. She hopes that her art can do the same for others whom may need a little piece of something pure positive and uplifting in their lives.
+            </p>
           </div>
           <br/>
             <a href="/contact"><h4>CONTACT YULIYA</h4></a>

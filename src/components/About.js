a4fbd1811d54d32b40df3da68d51f2fc8c2f2c33
Yuliya <jasenetska@gmail.com>
2019-10-29 19:05:16 -0400

    about/contact setup

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 var parse = require('html-react-parser');
 
-
 const About = (props) =>  {
       return(
         <div>
           <div id="about">
             {props.about.profileImage ? <img id="profile-pic" src={props.about.profileImage.fields.file.url} /> : ''}
             {props.about.paragraphs ? parse(props.about.paragraphs) : ''}
-            </div>
+          </div>
           <br/>
-            {props.about.cta ? <a href={props.about.cta}><h4>{props.about.ctaText}</h4></a> : ''}
+          {props.about.cta ? <a href={props.about.cta}><h4>{props.about.ctaText}</h4></a> : ''}
         </div>
       );
 };
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,17 +4,15 @@ const Contact = (props) =>  {
       return(
         <div id="contact-form">
           <center>
-          <b><h3>{props.title}</h3></b>
-          <p>{props.subtitle}</p>
-          
+            <b><h3>{props.title}</h3></b>
+            <p>{props.subtitle}</p>
           </center>
-          <iframe height="497"
-            frameBorder="0"
-            scrolling="no"
-            style={{width: '80%', border: 'none', display: 'block', margin: '0 auto', marginTop: '50px', backgroundColor: 'green !important'}}
-            src="https://yuliyasart.wufoo.com/embed/zfo8cs61rij29h/">
-
-          </iframe>
+            <iframe height="497"
+              frameBorder="0"
+              scrolling="no"
+              style={{width: '80%', border: 'none', display: 'block', margin: '0 auto', marginTop: '50px', backgroundColor: 'green !important'}}
+              src="https://yuliyasart.wufoo.com/embed/zfo8cs61rij29h/">
+            </iframe>
         </div>
       );
 };

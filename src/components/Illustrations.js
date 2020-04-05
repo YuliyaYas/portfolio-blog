3f2009c632de68186557293f16a172dc88a91af2
Yuliya <jasenetska@gmail.com>
2020-04-05 13:47:19 -0400

    added imgs to illustrations in order

--- src/components/Illustrations.js
+++ src/components/Illustrations.js
@@ -17,7 +17,7 @@ const Illustrations = (props) =>  {
           <div className="row-grid">
             <div className="column-grid">
             {
-           ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].map((i, count)=>{
+           ['53', '42', '49', '54', '46', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].map((i, count)=>{
             return <div><img key={count} src={require(`../imgs/illustrations/${data[i].url}`)} title={data[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} alt={data[0].name}/><div className="mobile-info">{data[i].sold ? <p className="sold-mobile" onClick={props.handleCloseClick}>SOLD</p> : <a href='/contact'><p className="label-mobile" onClick={props.handleCloseClick}>BUY</p></a>}<p className="all-caps">{data[i].name}</p><i><p className="title">{data[i].specs}</p></i><p className="desc">{data[i].description}</p></div></div>
             
           })
@@ -25,7 +25,7 @@ const Illustrations = (props) =>  {
             </div>
             <div className="column-grid">
             {
-           ['10', '11', '12', '13', '14', '15', '16', '17', '18'].map((i, count)=>{
+           [ '51', '47', '57', '43', '45', '10', '11', '12', '13', '14', '15', '16', '17', '18'].map((i, count)=>{
             return <div><img key={count} src={require(`../imgs/illustrations/${data[i].url}`)} title={data[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} alt={data[0].name}/><div className="mobile-info">{data[i].sold ? <p className="sold-mobile" onClick={props.handleCloseClick}>SOLD</p> : <a href='/contact'><p className="label-mobile" onClick={props.handleCloseClick}>BUY</p></a>}<p className="all-caps">{data[i].name}</p><i><p className="title">{data[i].specs}</p></i><p className="desc">{data[i].description}</p></div></div>
             
           })
@@ -33,14 +33,14 @@ const Illustrations = (props) =>  {
             </div>
             <div className="column-grid">
             {
-           ['19', '20', '21', '22', '23', '24', '25', '26', '27'].map((i, count)=>{
+           ['40', '38', '50', '56', '55', '39', '19', '20', '21', '22', '23', '24', '25', '26', '27'].map((i, count)=>{
             return <div><img key={count} src={require(`../imgs/illustrations/${data[i].url}`)} title={data[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} alt={data[0].name}/><div className="mobile-info">{data[i].sold ? <p className="sold-mobile" onClick={props.handleCloseClick}>SOLD</p> : <a href='/contact'><p className="label-mobile" onClick={props.handleCloseClick}>BUY</p></a>}<p className="all-caps">{data[i].name}</p><i><p className="title">{data[i].specs}</p></i><p className="desc">{data[i].description}</p></div></div>
           })
           }
           </div>
             <div className="column-grid">
             {
-           ['28', '29', '30', '31', '32', '33', '34', '35', '36', '37'].map((i, count)=>{
+           ['52', '41', '48', '44', '28', '29', '30', '31', '32', '33', '34', '35', '36', '37'].map((i, count)=>{
             return <div><img key={count} src={require(`../imgs/illustrations/${data[i].url}`)} title={data[0].name} name="illustrations" style={{width:'100%'}} onClick={props.handleImageClick} alt={data[0].name}/><div className="mobile-info">{data[i].sold ? <p className="sold-mobile" onClick={props.handleCloseClick}>SOLD</p> : <a href='/contact'><p className="label-mobile" onClick={props.handleCloseClick}>BUY</p></a>}<p className="all-caps">{data[i].name}</p><i><p className="title">{data[i].specs}</p></i><p className="desc">{data[i].description}</p></div></div>
           })
           }

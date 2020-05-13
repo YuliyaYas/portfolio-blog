28764345703b2f36fada589ec2565ecc56f286c5
Yuliya <jasenetska@gmail.com>
2020-05-13 14:09:28 -0400

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import {Route, Switch} from 'react-router-dom';
 import './App.css';
-import Home from '../src/components/Home.js';
+import HomePage from '../src/components/HomePage.js';
+import Menu from '../src/components/Menu.js';
+
 import About from '../src/components/About.js';
 import Paintings from '../src/components/Paintings.js';
 import Illustrations from '../src/components/Illustrations.js';
@@ -113,62 +115,22 @@ class App extends Component {
   render() {
     return (
       <div>
+        <Menu /> 
       <div className="content">
-      <div className="header-menu">
-        <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
-        <ul className="menu" id="menu-1">
-        <li><a href="/">HOME<i className="right"></i></a></li>
-        <li><a href="/paintings">PAINTINGS<i className="right"></i></a></li>
-        <li><a href="/illustrations">ILLUSTRATIONS<i className="right"></i></a></li>
-        <li><a href="/drawings">DRAWINGS<i className="right"></i></a></li>
-        <li><a href="/printmaking">PRINTMAKING<i className="right"></i></a></li>
-        {/* <li><a href="/sculptures">SCULPTURES<i className="right"></i></a></li> */}
-        <li><a href="/photography">PHOTOGRAPHY<i className="right"></i></a></li>
-        <li><a href="https://yuliyasart.redbubble.com/" target="_blank" rel="noopener noreferrer">SHOP PRINTS<i className="right"></i></a></li>
-        <li><a href="/about">ABOUT<i className="right"></i></a></li>
-        <li><a href="/contact">CONTACT<i className="right"></i></a></li>
-        <li><a href="https://www.instagram.com/yuliyas.art/" target="_blank" rel="noopener noreferrer">INSTAGRAM<i className="right"></i></a></li>
-        </ul>
+      
+          <Switch>
+            <Route path={`/contact`} component={ () => <Contact/>} />
+            <Route path={`/paintings/`} component={ () => <Paintings closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} paintingsData={paintingsData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />
+            <Route path={`/drawings`} component={ () => <Drawings closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} drawingsData={drawingsData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} />
+            <Route path={`/illustrations`} component={ () => <Illustrations closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} illustrationsData={illustrationsData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} />
+            <Route path={`/printmaking`} component={ () => <Printmaking closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} printmakingData={printmakingData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} />
+            {/* <Route path={`/sculptures`} component={ () => <Sculptures closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} sculpturesData={sculpturesData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} /> */}
+            <Route path={`/photography`} component={ () => <Photography closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} photographyData={photographyData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} />
+            <Route path={`/about`} component={ () => <About/>} />
+            <Route path={`/`} component={ () => <HomePage />} />
+          </Switch>
       </div>
-        <div className="row">
-          <div className="column-1">
-          <a href="/"><img id="logo" src={require("./imgs/logo_yy.png")} alt="logo"/></a>
-            <div id="menu">
-              <h4><a href="/paintings/">PAINTINGS<i className="right"></i></a></h4>
-              <h4><a href="/illustrations">ILLUSTRATIONS<i className="right"></i></a></h4>
-              <h4><a href="/drawings">DRAWINGS<i className="right"></i></a></h4>
-              <h4><a href="/printmaking">PRINTMAKING<i className="right"></i></a></h4>
-              {/* <h4><a href="/sculptures">SCULPTURES<i className="right"></i></a></h4> */}
-              <h4><a href="/photography">PHOTOGRAPHY<i className="right"></i></a></h4>
-              <h4><a href="https://yuliyasart.redbubble.com/" target="_blank" rel="noopener noreferrer">SHOP PRINTS<i className="right"></i></a></h4>
-            </div>
-            <Book/>
-            <div id="contact">
-              <h4 id="about"><a href="/">HOME</a></h4>
-              <h4 ><a href="/about">ABOUT</a></h4>
-              <h4><a href="/contact">CONTACT</a></h4>
-              <a href="https://www.instagram.com/yuliyas.art/" target="_blank" rel="noopener noreferrer"><h4>INSTAGRAM</h4></a>
-            </div>
-          </div>
-          <div className="column-2">
-            <Switch>
-              <Route path={`/contact`} component={ () => <Contact/>} />
-              <Route path={`/paintings/`} component={ () => <Paintings closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} paintingsData={paintingsData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />
-              <Route path={`/drawings`} component={ () => <Drawings closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} drawingsData={drawingsData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} />
-              <Route path={`/illustrations`} component={ () => <Illustrations closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} illustrationsData={illustrationsData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} />
-              <Route path={`/printmaking`} component={ () => <Printmaking closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} printmakingData={printmakingData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} />
-              {/* <Route path={`/sculptures`} component={ () => <Sculptures closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} sculpturesData={sculpturesData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} /> */}
-              <Route path={`/photography`} component={ () => <Photography closed={this.state.closed} info={this.state.info} galleryType={this.state.galleryType} photographyData={photographyData} handleArrowClick={this.handleArrowClick} handleImageClick={this.handleImageClick} handleCloseClick={this.handleCloseClick}/>} />} />
-              <Route path={`/about`} component={ () => <About/>} />
-              <Route path={`/`} component={ () => <Home />} />
-            </Switch>
-
-          </div>
-        </div>
-      </div>
-
-      </div>
+    </div>
     );
   }
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,7 +3,7 @@ import '../stylesheet/Menu.scss';
 
 const Menu = (props) => {
   return(
-    <div >
+    <div className="menu">
         <a href="/"><img id="logo" src={require("../imgs/logo_yy.png")} alt="logo"/></a>
         <ul className="menu-top">
         <li><a href="/">HOME</a></li>
@@ -17,13 +17,35 @@ const Menu = (props) => {
         {/* <li><a href="/photography">PHOTOGRAPHY</a></li> */}
         <li><a href="https://yuliyasart.redbubble.com/" target="_blank" rel="noopener noreferrer">SHOP</a></li>
         <li><a href="/about">ABOUT</a></li>
-        <li><a href="/contact">CONTACT</a></li>
+        <li>
+          <a href="/contact">
+              <img id="icons" src={require("../imgs/email.png")} alt="instagram"/>
+          </a>
+        </li>
         <li>
             <a href="https://www.instagram.com/yuliyas.art/" target="_blank" rel="noopener noreferrer">
-                <img id="insta" src={require("../imgs/ig.png")} alt="instagram"/>
+                <img id="icons" src={require("../imgs/ig.png")} alt="instagram"/>
             </a>
         </li>
         </ul>
+        <div className="mobile-menu">
+         <a href="/"><img id="logo-mobile" src={require("../imgs/logo_yy.png")} alt="logo"/></a>
+        <input className="menu-btn" type="checkbox" id="menu-btn" />
+        <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
+        <ul className="menu" id="menu-1">
+        <li><a href="/">HOME<i className="right"></i></a></li>
+        <li><a href="/paintings">PAINTINGS<i className="right"></i></a></li>
+        <li><a href="/illustrations">ILLUSTRATIONS<i className="right"></i></a></li>
+        <li><a href="/drawings">DRAWINGS<i className="right"></i></a></li>
+        <li><a href="/printmaking">PRINTMAKING<i className="right"></i></a></li>
+        {/* <li><a href="/sculptures">SCULPTURES<i className="right"></i></a></li> */}
+        <li><a href="/photography">PHOTOGRAPHY<i className="right"></i></a></li>
+        <li><a href="https://yuliyasart.redbubble.com/" target="_blank" rel="noopener noreferrer">SHOP PRINTS<i className="right"></i></a></li>
+        <li><a href="/about">ABOUT<i className="right"></i></a></li>
+        <li><a href="/contact">CONTACT<i className="right"></i></a></li>
+        <li><a href="https://www.instagram.com/yuliyas.art/" target="_blank" rel="noopener noreferrer">INSTAGRAM<i className="right"></i></a></li>
+        </ul>
+      </div>
     </div>
   );
 };

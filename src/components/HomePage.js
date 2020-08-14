29586254dd6c72d8f841eb8366bcfa023051f435
Yuliya <jasenetska@gmail.com>
2020-08-14 08:45:29 -0400

    bleh

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,17 +4,30 @@ import '../stylesheet/Homepage.scss';
 const HomePage = (props) => {
   return(
     <div className="homepage">
-      <div className="featured">        <h2>Latest works</h2>
-      <div className="block-img">
-        <img  src={require("../imgs/paintings/Snaility.jpg")} alt="Rebirth"/>
-      </div>
-      <div className="block-desc">
-        <div>SNAILITY</div>
-        <div> 20x20inches, acrylic on wood </div>
-        <br/>
-        The Greek poet Hesiod wrote that snails signified the time to harvest by climbing up the stems. This symbolized growth and rebirth. As women, we lead new innovations and ideas, we build new generations. We constantly grow and climb up the social ladder. Slowly but steady, small but many – may we rise up and carry our future to the brightest point.
-      </div>
-
+      <div className="featured">        
+        <h2>Latest works</h2>
+        <div className="featured-img">
+          <div className="block-img">
+            <img  src={require("../imgs/paintings/Snaility.jpg")} alt="Rebirth"/>
+          </div>
+          <div className="block-desc">
+            <div>SNAILITY</div>
+            <div> 20x20inches, acrylic on wood </div>
+            <br/>
+            The Greek poet Hesiod wrote that snails signified the time to harvest by climbing up the stems. This symbolized growth and rebirth. As women, we lead new innovations and ideas, we build new generations. We constantly grow and climb up the social ladder. Slowly but steady, small but many – may we rise up and carry our future to the brightest point.
+          </div>
+        </div>
+        <div className="featured-img">
+          <div className="block-img">
+            <img  src={require("../imgs/paintings/Snaility.jpg")} alt="Rebirth"/>
+          </div>
+          <div className="block-desc">
+            <div>SNAILITY</div>
+            <div> 20x20inches, acrylic on wood </div>
+            <br/>
+            The Greek poet Hesiod wrote that snails signified the time to harvest by climbing up the stems. This symbolized growth and rebirth. As women, we lead new innovations and ideas, we build new generations. We constantly grow and climb up the social ladder. Slowly but steady, small but many – may we rise up and carry our future to the brightest point.
+          </div>
+        </div>
       </div>
     </div>
   );

3ae94167a5ced81c206d7ae507d0b5b47fdd06b1
Yuliya <jasenetska@gmail.com>
2020-11-01 11:54:31 -0500

    refactoring

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -25,65 +25,29 @@ const EmblaCarousel = () => {
     onSelect();
   }, [embla, onSelect]);
 
+  const carouselImages = [
+    {url: 'MushTown.jpg', name: 'Colony, 24x30inches, acrylic on wood, 2020'},
+    {url: 'TurtleVille.jpg', name: 'World Bearing, 36x24inches, oil on canvas, 2016'},
+    {url: 'Snaility.jpg', name: 'Snaility, 20x20inches, acrylic on wood, 2017'},
+    {url: 'Cicada.jpg', name: 'Cicada, 18x24inches, acrylic and golden leaf on wood, 2020'},
+    {url: 'Glow.jpg', name: ''},
+
+  ]
   return (
     <div className="embla">
       <div className="embla__viewport" ref={viewportRef}>
         <div className="embla__container">
-            <div className="embla__slide">
+          {carouselImages.map(art => {return <div className="embla__slide">
               <div className="embla__slide__inner">
                 <img
                   className="embla__slide__img"
-                  src={require("../imgs/paintings/MushTown.jpg")}
+                  src={require(`../imgs/paintings/${art.url}`)}
                   alt="Colony"
                 />
-                 <p className="selected-img-desc">Colony, 24x30inches, acrylic on wood, 2020</p>
+                 <p className="selected-img-desc">{art.name}</p>
               </div>
-
-            </div>
-
-            <div className="embla__slide">
-              <div className="embla__slide__inner">
-                <img
-                  className="embla__slide__img"
-                  src={require("../imgs/paintings/TurtleVille.jpg")}
-                  alt="turtle"
-                />
-                 <p className="selected-img-desc">World Bearing, 36x24inches, oil on canvas, 2016</p>
-              </div>
-            </div>
-
-            <div className="embla__slide">
-              <div className="embla__slide__inner">
-                <img
-                  className="embla__slide__img"
-                  src={require("../imgs/paintings/Snaility.jpg")}
-                  alt="Snail"
-                />
-                 <p className="selected-img-desc">Snaility, 20x20inches, acrylic on wood, 2017</p>
-              </div>
-            </div>
-           
-            <div className="embla__slide">
-              <div className="embla__slide__inner">
-                <img
-                  className="embla__slide__img"
-                  src={require("../imgs/paintings/Cicada.jpg")}
-                  alt="Cicada"
-                />
-              <p className="selected-img-desc">Cicada, 18x24inches, acrylic and golden leaf on wood, 2020</p>
-              </div>
-            </div>
-            <div className="embla__slide">
-              <div className="embla__slide__inner">
-                <img
-                  className="embla__slide__img"
-                  src={require("../imgs/paintings/Glow.jpg")}
-                  alt="Jellyfish"
-                />
-                <p className="selected-img-desc">Glow, 24x30inches, acrylic on wood, 2020</p>
-                
-              </div>
-            </div>
+            </div>})}
+            
         </div>
       </div>
       <PrevButton onClick={scrollPrev} enabled={prevBtnEnabled} />
